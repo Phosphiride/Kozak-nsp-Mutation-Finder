eb66bb0b88701cd947b9f7040bc1c5e1ca165157
Phosphiride <64817854+Phosphiride@users.noreply.github.com>
2022-08-07 18:25:49 -0400

    added ability to create new folders for every run

--- createmutfiles.py
+++ createmutfiles.py
@@ -3,6 +3,7 @@ import io
 import re
 import subprocess
 from glob import glob
+from datetime import datetime
 
 import Bio.SeqIO
 from createmutationfile import createMutFile
@@ -11,27 +12,34 @@ def createmutfiles(inputfastas, outputfilenames, unaggoutputs, countryaggs, para
     for inputfasta, outputfilename, unaggoutput, countryagg in zip(inputfastas, outputfilenames, unaggoutputs, countryaggs):
         createMutFile(inputfasta, outputfilename, unaggoutput, countryagg, parameter)
 
-def createfolder():
-    pass
+def createfolders():
+    filedate = datetime.today().strftime('%Y%m%d%H%M%S')
+    outputpath = f'result/{filedate}/output'
+    countrypath = f'result/{filedate}/country'
+    unaggpath = f'result/{filedate}/unagg'
+    for path in [outputpath, countrypath, unaggpath]:
+        os.makedirs(path)
+    return outputpath, countrypath, unaggpath
 
-def outputname(inputfastas):
+def outputname(inputfastas, outputpath, countrypath, unaggpath):
     output_list = []
     country_list = []
     unagg_list = []
     count = 1
     for file in inputfastas:
-        output_list.append(f'result/multtest_{count}.csv')
-        unagg_list.append(f'result/unagg_{count}.csv')
-        country_list.append(f'result/country_{count}.csv')
+        output_list.append(f'{outputpath}/multtest_{count}.csv')
+        unagg_list.append(f'{countrypath}/unagg_{count}.csv')
+        country_list.append(f'{unaggpath}/country_{count}.csv')
         count += 1
     return output_list, country_list, unagg_list
 
 if __name__ == '__main__':
     #inputfastas = ["data/20211104_gisaid_genomes.fasta", "data/20210613_gisaid_genomes.fasta"]
     inputfastas = glob("./data/multfolder/*.fasta")
+    outputpath, countrypath, unaggpath = createfolders()
 
     #outputfilenames = ['result/multtest_1.csv', 'result/multtest_2.csv']
-    outputfilenames, countryaggs, unaggoutputs = outputname(inputfastas)
+    outputfilenames, countryaggs, unaggoutputs = outputname(inputfastas, outputpath, countrypath, unaggpath)
     #unaggoutputs = ['result/unagg_1.csv', 'result/unagg_2.csv']
     #countryaggs = ['result/country_1.csv', 'result/country_2.csv']
     wildtype = "data/GISAID_nsp5.fasta"
