a20fd9023a25ac87bcc8881824dd2a57f42e1d63
Phosphiride <64817854+Phosphiride@users.noreply.github.com>
2022-08-13 21:28:54 -0400

    improved fasta splitter to actually split fasta files as whole sequences.

--- fastasplitter.py
+++ fastasplitter.py
@@ -1,7 +1,9 @@
 import os
+import pandas
 
-def splitfasta(inputfasta, outputpath, outputfastas, parameter):
+def fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter):
     splitfile = None
+    linesperfile = parameter['linesperfile']
 
     isExist = os.path.exists(outputpath)
 
@@ -12,24 +14,39 @@ def splitfasta(inputfasta, outputpath, outputfastas, parameter):
 
     with open(inputfasta) as input:
         for lineno, line in enumerate(input):
-            if splitfile:
-                splitfile.close()
-            split_filename = outputfastas.format(int(lineno/parameter['linesperfile'] + 1))
-            splitfile = open(split_filename, 'w')
-            print(f'creating {split_filename}')
-        splitfile.write(line)
-        count += 1
-    if splitfile:
-        splitfile.close()
-        count += 1
+            if line.startswith('>hCoV-19/'):
+                print(line)
 
-    print(f'Finished creating {count} files.')
+                if count % parameter['linesperfile'] == 0:
+                    if splitfile:
+                        splitfile.close()
+                    split_filename = outputfastas.format(file_name, file_name, int(count / linesperfile + 1))
+                    splitfile = open(split_filename, 'w')
+                    print(f'creating {split_filename}')
+
+                count += 1
+            splitfile.write(line)
+
+    if not splitfile.closed:
+        splitfile.close()
+
+    print(f'Finished creating {int(count / linesperfile)} files.')
+
+def metasplit(inputmeta):
+    meta_db = p
+
+def getmetaname(inputname):
+    name_split = inputname.split('.')
+    meta_name = f"{name_split[0]}.metadata.tsv"
+    file_name = name_split[0].split('/')
+    return meta_name, file_name[2]
 
 
 if __name__ == '__main__':
-    inputfasta = "data/spikenuc0523_1.fasta"
-    outputpath = "data/spikenuc0523_1"
-    outputfastas = "data/spikenuc0523_1/spikenuc0523_1_{}.fasta"
-    parameter = {'linesperfile': 2500}
+    inputfasta = "data/test2/1660403438836.sequences.fasta"
+    inputmeta, file_name = getmetaname(inputfasta)
+    outputpath = f"data/{file_name}"
+    outputfastas = "data/{}/{}_{}.fasta"
+    parameter = {'linesperfile': 3}
 
-    splitfasta(inputfasta, outputpath, outputfastas, parameter)
+    fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter)
