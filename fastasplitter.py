ee90e6761d5663aa07bf73a1cf9177cefe1465b8
Phosphiride <64817854+Phosphiride@users.noreply.github.com>
2022-08-16 16:05:44 -0400

    fixed bug in fasta splitter

--- fastasplitter.py
+++ fastasplitter.py
@@ -1,4 +1,14 @@
 import os
+import re
+
+def dnatest(test):
+    out = re.search("^[ATCGMRWSYKVHDBN]+$", test)
+
+    if out:
+        return True
+
+    else:
+        return False
 
 def fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter):
     splitfile = None
@@ -11,11 +21,14 @@ def fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter):
     if not isExist:
         os.makedirs(outputpath)
 
-    with open(inputfasta) as input:
-        for lineno, line in enumerate(input):
-            if line.startswith('>NSP5|hCoV-19'):
-                print(line)
+    with open(inputfasta) as inputfasta_fp:
+        line = inputfasta_fp.readline()
+        while line:
+            if dnatest(line) == True and header_type == 1:
+                splitfile.write(line)
 
+            elif line.startswith('>NSP5|hCoV-19'):
+                header_type = 1
                 if count % parameter['linesperfile'] == 0:
                     if splitfile:
                         splitfile.close()
@@ -26,10 +39,37 @@ def fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter):
                 count += 1
                 splitfile.write(line)
 
+            else:
+                header_type = 0
+
+            line = inputfasta_fp.readline()
+
     if not splitfile.closed:
         splitfile.close()
 
-    print(f'Finished creating {int(count / linesperfile)} files.')
+
+        '''for lineno, line in enumerate(input):
+            if line.startswith('>NSP5|hCoV-19'):
+
+                if count % parameter['linesperfile'] == 0:
+                    if splitfile:
+                        splitfile.close()
+                    split_filename = outputfastas.format(file_name, file_name, int(count / linesperfile + 1))
+                    splitfile = open(split_filename, 'w')
+                    print(f'creating {split_filename}')
+
+                count += 1
+                splitfile.write(line)
+                
+                if dnatest(line) == True:
+                    splitfile.write(line)
+
+
+
+    if not splitfile.closed:
+        splitfile.close()
+
+    print(f'Finished creating {int(count / linesperfile)} files.')'''
 
 def getfilename(inputname):
     name_split = inputname.split('.')
@@ -42,6 +82,6 @@ if __name__ == '__main__':
     file_name = getfilename(inputfasta)
     outputpath = f"data/{file_name}"
     outputfastas = "data/{}/{}_{}.fasta"
-    parameter = {'linesperfile': 3}
+    parameter = {'linesperfile': 4}
 
     fastasplit(inputfasta, outputpath, outputfastas, file_name, parameter)
